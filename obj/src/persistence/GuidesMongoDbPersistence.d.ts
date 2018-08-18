ed84179e06143455626140cd05be164f9e6d5911
pipdeveloper <pipdevs@gmail.com>
2018-08-18 06:59:28 +0000

    project build by Travis CI [skip ci]

--- obj/src/persistence/GuidesMongoDbPersistence.d.ts
+++ obj/src/persistence/GuidesMongoDbPersistence.d.ts
@@ -5,7 +5,7 @@ import { GuideV1 } from '../data/version1/GuideV1';
 import { IGuidesPersistence } from './IGuidesPersistence';
 export declare class GuidesMongoDbPersistence extends IdentifiableMongoDbPersistence<GuideV1, string> implements IGuidesPersistence {
     constructor();
-    private composeFilter;
+    private composeFilter(filter);
     getPageByFilter(correlationId: string, filter: FilterParams, paging: PagingParams, callback: any): void;
     getOneRandom(correlationId: string, filter: FilterParams, callback: (err: any, item: GuideV1) => void): void;
 }
