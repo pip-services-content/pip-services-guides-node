5298cb766e812458e09a6e6adcbf6bda8279fb0f
Dmitriy Krayniy <krdima92@gmail.com>
2018-08-20 14:04:18 +0300

    added DefaultRpcFactory

--- obj/src/container/GuidesProcess.js
+++ obj/src/container/GuidesProcess.js
@@ -2,10 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const pip_services_container_node_1 = require("pip-services-container-node");
 const GuidesServiceFactory_1 = require("../build/GuidesServiceFactory");
+const pip_services_rpc_node_1 = require("pip-services-rpc-node");
 class GuidesProcess extends pip_services_container_node_1.ProcessContainer {
     constructor() {
         super("guides", "System guides microservice");
         this._factories.add(new GuidesServiceFactory_1.GuidesServiceFactory);
+        this._factories.add(new pip_services_rpc_node_1.DefaultRpcFactory);
     }
 }
 exports.GuidesProcess = GuidesProcess;
--- src/container/GuidesProcess.ts
+++ src/container/GuidesProcess.ts
@@ -2,12 +2,14 @@ import { IReferences } from 'pip-services-commons-node';
 import { ProcessContainer } from 'pip-services-container-node';
 
 import { GuidesServiceFactory } from '../build/GuidesServiceFactory';
+import { DefaultRpcFactory } from 'pip-services-rpc-node';
 
 export class GuidesProcess extends ProcessContainer {
 
     public constructor() {
         super("guides", "System guides microservice");
         this._factories.add(new GuidesServiceFactory);
+        this._factories.add(new DefaultRpcFactory);
     }
 
 }
