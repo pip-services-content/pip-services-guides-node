d225619414a9e6b38fcc240090416c2d5e2ca92a
pipdeveloper <pipdevs@gmail.com>
2019-04-23 12:55:13 +0000

    project build by Travis CI [skip ci]

--- obj/src/data/version1/GuideTypeV1.d.ts
+++ obj/src/data/version1/GuideTypeV1.d.ts
@@ -1,5 +1,5 @@
 export declare class GuideTypeV1 {
-    static readonly Introduction = "introduction";
-    static readonly NewRelease = "new release";
-    static readonly Home = "home";
+    static readonly Introduction: string;
+    static readonly NewRelease: string;
+    static readonly Home: string;
 }
